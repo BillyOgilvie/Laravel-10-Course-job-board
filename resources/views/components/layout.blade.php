3608d90af5653b1abdd41e1676471a83e4b2a91e
Billy Ogilvie <billy.ogilvie@drewberry.co.uk>
2024-07-04 08:05:21 +0000

    105. Filtering Jobs: Tailwind Form Plugin & Text Inputs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel Job Board</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
