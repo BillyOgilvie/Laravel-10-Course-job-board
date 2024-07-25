e7c4f06705b118fece7217ed7cb9df55627a269e
Billy Ogilvie <billy.ogilvie@drewberry.co.uk>
2024-07-25 07:55:25 +0000

    113. Refactor Time: Gradient Background, Styling Buttons, Adding Alpine.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,13 @@
+import axios from 'axios';
+import Alpine from 'alpinejs';
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -30,3 +33,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+
+
+window.Alpine = Alpine
+
+Alpine.start();
